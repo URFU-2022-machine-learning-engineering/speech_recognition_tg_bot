cdba46513d5e11cc844783faafa55c5c631e7d9b
OrlovAlexandr <eaglophone@gmail.com>
2023-06-18 22:47:59 +0500

    Updated main.py: Uploading file to web-server implemented Updated requirements.txt

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import os
+
+import requests
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from aiogram.utils import executor
-import config
+from requests_toolbelt.multipart.encoder import MultipartEncoder
 
+import config
 
 # Инициализация бота и диспетчера
 bot = Bot(token=config.TOKEN)
 storage = MemoryStorage()
 dp = Dispatcher(bot, storage=storage)
+
+# URL веб-сервера
+web_server_url = config.URL
 
 
 @dp.message_handler(content_types=types.ContentType.VOICE)
@@ -21,19 +28,35 @@ async def process_voice_message(message: types.Message, state: FSMContext):
     voice_file_data = await bot.download_file(voice_file_path)
 
     # Сохранение файла в формате mp3 на сервере
-    mp3_file_path = 'E:/file_1.mp3'
+    mp3_file_path = 'E:/file_3.mp3'
     with open(mp3_file_path, 'wb') as mp3_file:
         mp3_file.write(voice_file_data.getvalue())
 
     # Отправка ответного сообщения
-    await message.reply('Голосовое сообщение сохранено в формате MP3')
+    await message.reply('Голосовое сообщение сохранено')
+
+    # Отправка голосового сообщения на веб-сервер
+    multipart_data = MultipartEncoder(
+        fields={'file': (os.path.basename(mp3_file_path), open(mp3_file_path, 'rb'), 'audio/mpeg')}
+    )
+    headers = {'Content-Type': multipart_data.content_type}
+
+    response = requests.post(web_server_url, data=multipart_data, headers=headers)
+
+    if response.status_code == 200:
+        # Если запрос успешен, отправляем ответное сообщение от сервера в Telegram
+        response_data = response.json()
+        detected_language = response_data['detected_language']
+        recognized_text = response_data['recognized_text']
+        await message.reply(f'Detected Language: {detected_language}\nRecognized Text: {recognized_text}')
+    else:
+        await message.reply('Ошибка при отправке голосового сообщения на сервер')
 
 
 @dp.message_handler(Command('start'))
 async def start_command(message: types.Message):
-    await message.reply('Привет! Отправь мне голосовое сообщение, и я сохраню его в формате MP3.')
+    await message.reply('Отправь мне голосовое сообщение, и я передам его на распознавание')
 
 
 if __name__ == '__main__':
-    # Запуск бота
     executor.start_polling(dp, skip_updates=True)
