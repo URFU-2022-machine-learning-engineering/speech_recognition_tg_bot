7f3e9f0b8d9f9f73e226dc73a4b3f9bd15ac5050
Vladimir Katin <katin.v.v@gmail.com>
2023-06-19 14:02:19 +0300

    add pre-commit hooks

--- main.py
+++ main.py
@@ -28,35 +28,33 @@ async def process_voice_message(message: types.Message, state: FSMContext):
     voice_file_data = await bot.download_file(voice_file_path)
 
     # Сохранение файла в формате mp3 на сервере
-    mp3_file_path = 'E:/file_3.mp3'
-    with open(mp3_file_path, 'wb') as mp3_file:
+    mp3_file_path = config.PATH
+    with open(mp3_file_path, "wb") as mp3_file:
         mp3_file.write(voice_file_data.getvalue())
 
     # Отправка ответного сообщения
-    await message.reply('Голосовое сообщение сохранено')
+    await message.reply("Голосовое сообщение сохранено")
 
     # Отправка голосового сообщения на веб-сервер
-    multipart_data = MultipartEncoder(
-        fields={'file': (os.path.basename(mp3_file_path), open(mp3_file_path, 'rb'), 'audio/mpeg')}
-    )
-    headers = {'Content-Type': multipart_data.content_type}
+    multipart_data = MultipartEncoder(fields={"file": (os.path.basename(mp3_file_path), open(mp3_file_path, "rb"), "audio/mpeg")})
+    headers = {"Content-Type": multipart_data.content_type}
 
     response = requests.post(web_server_url, data=multipart_data, headers=headers)
 
     if response.status_code == 200:
         # Если запрос успешен, отправляем ответное сообщение от сервера в Telegram
         response_data = response.json()
-        detected_language = response_data['detected_language']
-        recognized_text = response_data['recognized_text']
-        await message.reply(f'Detected Language: {detected_language}\nRecognized Text: {recognized_text}')
+        detected_language = response_data["detected_language"]
+        recognized_text = response_data["recognized_text"]
+        await message.reply(f"Detected Language: {detected_language}\nRecognized Text: {recognized_text}")
     else:
-        await message.reply('Ошибка при отправке голосового сообщения на сервер')
+        await message.reply("Ошибка при отправке голосового сообщения на сервер")
 
 
-@dp.message_handler(Command('start'))
+@dp.message_handler(Command("start"))
 async def start_command(message: types.Message):
-    await message.reply('Отправь мне голосовое сообщение, и я передам его на распознавание')
+    await message.reply("Отправь мне голосовое сообщение, и я передам его на распознавание")
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     executor.start_polling(dp, skip_updates=True)
