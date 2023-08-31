cb51c01491f82d46273a978916e2a9c95bb38c31
Vladimir Katin <51898657+dzailz@users.noreply.github.com>
2023-08-31 23:25:15 +0300

    ready to deploy (#5)
    * huge refactoring
    
    * add Dockerfile, add logging
    
    * add Jenkinsfile, add docker build and push

--- main.py
+++ main.py
@@ -1,60 +1,127 @@
+import logging
 import os
+import tempfile
 
 import requests
 from aiogram import Bot, Dispatcher, types
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
-from aiogram.dispatcher import FSMContext
 from aiogram.dispatcher.filters import Command
 from aiogram.utils import executor
 from requests_toolbelt.multipart.encoder import MultipartEncoder
 
-import config
-
-# Инициализация бота и диспетчера
-bot = Bot(token=config.TOKEN)
-storage = MemoryStorage()
-dp = Dispatcher(bot, storage=storage)
-
-# URL веб-сервера
-web_server_url = config.URL
+logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
 
 
-@dp.message_handler(content_types=types.ContentType.VOICE)
-async def process_voice_message(message: types.Message, state: FSMContext):
-    # Сохранение голосового сообщения в файл
-    voice_file_id = message.voice.file_id
-    voice_file = await bot.get_file(voice_file_id)
-    voice_file_path = voice_file.file_path
-    voice_file_data = await bot.download_file(voice_file_path)
+class FileManager:
+    @staticmethod
+    async def save_voice_message(voice_file_data):
+        # Save voice message to a temporary file
+        with tempfile.NamedTemporaryFile(delete=False) as temp_file:
+            temp_file.write(voice_file_data.read())
+            temp_file_path = temp_file.name
+            logging.debug(f"Saved voice message to {temp_file_path}")
+        return temp_file_path
 
-    # Сохранение файла в формате mp3 на сервере
-    mp3_file_path = config.PATH
-    with open(mp3_file_path, "wb") as mp3_file:
-        mp3_file.write(voice_file_data.getvalue())
-
-    # Отправка ответного сообщения
-    await message.reply("Голосовое сообщение сохранено")
-
-    # Отправка голосового сообщения на веб-сервер
-    multipart_data = MultipartEncoder(fields={"file": (os.path.basename(mp3_file_path), open(mp3_file_path, "rb"), "audio/mpeg")})
-    headers = {"Content-Type": multipart_data.content_type}
-
-    response = requests.post(web_server_url, data=multipart_data, headers=headers)
-
-    if response.status_code == 200:
-        # Если запрос успешен, отправляем ответное сообщение от сервера в Telegram
-        response_data = response.json()
-        detected_language = response_data["detected_language"]
-        recognized_text = response_data["recognized_text"]
-        await message.reply(f"Detected Language: {detected_language}\nRecognized Text: {recognized_text}")
-    else:
-        await message.reply("Ошибка при отправке голосового сообщения на сервер")
+    @staticmethod
+    async def delete_temp_file(temp_file_path):
+        if os.path.exists(temp_file_path):
+            logging.debug(f"Deleted temporary file {temp_file_path}")
+            os.remove(temp_file_path)
 
 
-@dp.message_handler(Command("start"))
-async def start_command(message: types.Message):
-    await message.reply("Отправь мне голосовое сообщение, и я передам его на распознавание")
+class VoiceBot:
+    def __init__(self, token: str, url: str):
+        self.token = token if token else os.getenv("TOKEN")
+        self.url = url if url else os.getenv("URL")
+        self.bot = Bot(token=self.token)
+        self.storage = MemoryStorage()
+        self.dp = Dispatcher(bot=self.bot, storage=self.storage)
+        self.file_manager = FileManager()
+        logging.debug(f"Voice bot initiated to URL {self.url}")
+
+    async def send_voice_message(self, message, temp_file_path):
+        try:
+            with open(temp_file_path, "rb") as file:
+                multipart_data = MultipartEncoder(fields={"file": (os.path.basename(temp_file_path), file, "audio/mpeg")})
+                headers = {"Content-Type": multipart_data.content_type}
+                response = requests.post(self.url, data=multipart_data, headers=headers)
+                logging.debug(f"Request sent to {self.url}, received response code {response.status_code} " f"and response text {response.text}")
+                response_data = response.json()
+                detected_language = response_data["detected_language"]
+                recognized_text = response_data["recognized_text"]
+                await self.send_response_with_buttons(
+                    message=message,
+                    detected_language=detected_language,
+                    recognized_text=recognized_text,
+                )
+        except (requests.RequestException, FileNotFoundError) as e:
+            logging.error(f"There was an error during request {e}")
+            await self.send_error_response_with_buttons(message)
+
+    @staticmethod
+    async def send_response_with_buttons(message, detected_language, recognized_text):
+        keyboard = types.InlineKeyboardMarkup(row_width=2)
+        retry_button = types.InlineKeyboardButton("Retry", callback_data="retry")
+        cancel_button = types.InlineKeyboardButton("Cancel", callback_data="cancel")
+        keyboard.add(retry_button, cancel_button)
+
+        await message.reply(
+            f"Detected Language: {detected_language}\nRecognized Text: {recognized_text}",
+            reply_markup=keyboard,
+        )
+
+    @staticmethod
+    async def send_error_response_with_buttons(message):
+        keyboard = types.InlineKeyboardMarkup()
+        retry_button = types.InlineKeyboardButton("Retry", callback_data="retry")
+        cancel_button = types.InlineKeyboardButton("Cancel", callback_data="cancel")
+        keyboard.add(retry_button, cancel_button)
+
+        await message.reply(
+            "Ошибка при отправке голосового сообщения на сервер",
+            reply_markup=keyboard,
+        )
+
+    def run(self):
+        @self.dp.message_handler(content_types=[types.ContentType.AUDIO, types.ContentType.VOICE])
+        async def process_voice_message(message: types.Message):
+            # Download audio/voice message
+            if message.audio:
+                audio_file_id = message.audio.file_id
+            else:
+                audio_file_id = message.voice.file_id
+
+            audio_file = await self.bot.get_file(audio_file_id)
+            audio_file_path = audio_file.file_path
+            audio_file_data = await self.bot.download_file(audio_file_path)
+
+            # Save audio/voice message and send it for processing
+            temp_file_path = await self.file_manager.save_voice_message(audio_file_data)
+            await self.send_voice_message(message, temp_file_path)
+
+        @self.dp.callback_query_handler(lambda c: c.data == "retry")
+        async def process_retry_callback(callback_query: types.CallbackQuery):
+            await self.bot.answer_callback_query(callback_query.id)
+            await self.bot.delete_message(callback_query.message.chat.id, callback_query.message.message_id)
+            await self.send_voice_message(callback_query.message, callback_query.from_user.id)
+
+        @self.dp.callback_query_handler(lambda c: c.data == "cancel")
+        async def process_cancel_callback(callback_query: types.CallbackQuery):
+            await self.bot.answer_callback_query(callback_query.id)
+            await self.bot.delete_message(callback_query.message.chat.id, callback_query.message.message_id)
+            await self.file_manager.delete_temp_file(callback_query.from_user.id)
+
+        @self.dp.message_handler(Command("start"))
+        async def start_command(message: types.Message):
+            logging.debug("User has launched the bot")
+            await message.reply("Отправь мне голосовое сообщение, и я передам его на распознавание")
+
+        executor.start_polling(self.dp, skip_updates=True)
 
 
 if __name__ == "__main__":
-    executor.start_polling(dp, skip_updates=True)
+    logging.basicConfig(level=logging.DEBUG, format="%(asctime)s - %(levelname)s - %(message)s")
+    logging.getLogger("__name__")
+
+    voice_bot = VoiceBot(token=os.getenv("TOKEN"), url=os.getenv("URL"))
+    voice_bot.run()
